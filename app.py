070acef2d24d3b3202d19e4d4988046d03e0ba9c
wubbqt <54856442+wubbqt@users.noreply.github.com>
2022-08-28 23:29:25 -0400

    starting

---
+++ app.py
@@ -0,0 +1,16 @@
+from constants import TEAMS
+from constants import PLAYERS
+
+
+#sudo
+#take existing data from players and add it to a new dictionary in a list
+#while doing that clean the height, experiance, gaurdian
+
+
+
+
+
+
+def cleaned_players(PLAYERS):
+    cleaned = []
+    for player in PLAYERS:
