3b8072bb1789e4961a5c8437643e47a95d35671d
wubbqt <54856442+wubbqt@users.noreply.github.com>
2022-09-06 10:18:35 -0700

    Updates

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from multiprocessing.sharedctypes import Value
 import constants
 import copy
 import random
@@ -12,7 +11,9 @@ new_players = copy.deepcopy(constants.PLAYERS)
 new_teams = copy.deepcopy(constants.TEAMS)
 experience_yes = []
 experience_no = []
-
+panthers = []
+bandits = []
+warriors = []
 
 def clean_data():
     for player in new_players:
@@ -33,12 +34,9 @@ def clean_data():
 
 
 def balance_teams():
-    panthers = []
-    bandits = []
-    warriors = []
-    all_teams = []
     random.shuffle(experience_yes)
     random.shuffle(experience_no)
+
     while experience_yes:
         warriors.append(experience_yes.pop())
         bandits.append(experience_yes.pop())
@@ -48,41 +46,85 @@ def balance_teams():
         warriors.append(experience_no.pop())
         bandits.append(experience_no.pop())
         panthers.append(experience_no.pop())
-    all_teams = [warriors, bandits, panthers]
-    return all_teams
+    return panthers, bandits, warriors
 
 
+def team_stats(team):
+    player_count = len(team)
+    experienced_count = 0
+    inexperienced_count = 0
+    total_height = 0
+    names_players = []
+    gaurdians_team = []
+    for player in team:
+        if player ["experience"] == True:
+            experienced_count += 1
+        elif player["experience"] == False:
+            inexperienced_count += 1
+    for player in team:
+        total_height += player["height"]
+    average_height = round(total_height / len(team))
+    for player in team:
+        names_players.append(player['name'])
+        gaurdians_team.append(player['guardians'])
+
+    
 
 
+    print("\n-------------\n")
+    print("Total Players: ", player_count)
+    print("Total Experienced: ", experienced_count)
+    print("Total Inexperienced: ", inexperienced_count)
+    print("Average Height: ", average_height)
+    print("Player Names: ")
+    print(', '.join(names_players))
+    print("Gaurdian Names: ")
+    print(', '.join(gaurdians_team))
+    
 
 
 def main_menu():
-    print("BASKETBALL TEAM STATS TOOL\n\n\n")
-    print("---MENU---\n\n")
     
-    try:
-        while True:
-            menu1 = input(
-                "Here are your choices: \n A) Display Team Stats \n B) Quit\n")
-            if menu1 == "a" or "A":
-                team_choice()
-            if menu1 == "b" or "B":
-                print("Quiting Application")
-                break
-
-    except ValueError:
-        print("Invalid choicem, please try again.")
-        
+    while True:
+        user_option = input("BASKETBALL TEAM STATS TOOL: \n\n"
+                            "---MENU---\n\n"
+                            "Here are your choices:\n"
+                            "A) Display Team Stats\n"
+                            "B) Quit\n Enter an option: \n").upper()
+        if user_option == "A":
+            print(
+                "A) Panthers\n"
+                "B) Warriors\n"
+                "C) Bandits\n")
             
+            team_option = input("Select a team from above\n").upper()
+            if team_option == "A":
+                print("\n\nDisplaying Panther Stats")
+                team_stats(panthers)
+            elif team_option == "B":
+                print("\n\nDisplaying Warriors Stats")
+                team_stats(warriors)
+            elif team_option == "C":
+                print("\n\nDisplaying Bandits Stats")
+                team_stats(bandits)
+            else:
+                print("Invalid Selection")
+        elif user_option == "B":
+            print("Okay bye...")
+            break
+        else:
+            print("Invalid Selection")
+    
 
 
 
-            
-            
-def team_choice():
-    print("A) Panthers\nB) Bandits\nC) Warriors\n")
+
+
+#def team_stats():
+
 
             
 if __name__ == "__main__":
     clean_data()
+    balance_teams()
     main_menu()
